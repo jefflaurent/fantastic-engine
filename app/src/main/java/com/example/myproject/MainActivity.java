1a5e4a1f571e9fc4410e45d24668f890ddd781a9
Jeff <jefflaurent19@gmail.com>
2020-03-02 19:07:00 +0700

    calculator

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -2,14 +2,39 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    TextView hasil;
+    EditText angka1, angka2;
+    Button clickBtn;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("test");
+        angka1 = findViewById(R.id.angka1);
+        angka2 = findViewById(R.id.angka2);
+        hasil = findViewById(R.id.hasil);
+        clickBtn = findViewById(R.id.btnClick);
+
+        clickBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId()){
+            case R.id.btnClick:
+                int res = Integer.parseInt(angka1.getText().toString()) + Integer.parseInt(angka2.getText().toString());
+                hasil.setText(res+"");
+                break;
+        }
     }
 }
